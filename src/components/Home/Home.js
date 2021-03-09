70d6057936331d94340cc0e3983e6aa7675788d4
bulbucus <emanuelfarinha98@gmail.com>
2021-03-09 03:53:29 +0000

    add background to home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import {ReactComponent as Background} from '../../assets/inicialPag/background2.svg'
+
+import './Home.scss'
+
+const Home = () => {
+
+    return (
+        <>
+            <Background className="backgroundHome"></Background>
+        </>
+    )
+}
+
+export default Home;
